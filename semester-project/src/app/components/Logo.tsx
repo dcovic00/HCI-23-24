a2ada1511697807b4a4348cccfd3ec88f4a7c272
dcovic00 <147554469+dcovic00@users.noreply.github.com>
2024-01-08 16:20:45 +0100

    Delete semester-project directory

--- semester-project/src/app/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import Image from 'next/image'
-import logo from 'public/images/logo.png' 
-
-const Logo: FC = () => (
-    <Image src={logo} alt="Logo"/>
-);
-
-export default Logo;
--- semester-project/src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import NotFound404 from './_components/notFound404/NotFound404';
-
-export default function NotFound() {
-  return (
-    <NotFound404/>
-  );
-};
-
